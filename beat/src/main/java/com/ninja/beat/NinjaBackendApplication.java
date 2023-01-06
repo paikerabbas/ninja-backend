7f3af43b8a0c6b554beb821e04b1743a4862d162
amarhusain <amar.husain156@gmail.com>
2023-01-06 21:08:00 +0530

    second commit

---
+++ beat/src/main/java/com/ninja/beat/NinjaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ninja.beat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaBackendApplication.class, args);
+	}
+
+}
---
+++ beat/src/test/java/com/ninja/beat/NinjaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninja.beat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
